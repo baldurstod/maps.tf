d27b683d2061ffa78c6c5d5f7430beb59fc5cc1d
Guillaume <108530065+baldurstod@users.noreply.github.com>
2025-01-31 21:17:59 +0100

    Added typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import copy from 'rollup-plugin-copy';
 import css from 'rollup-plugin-import-css';
+import typescript from '@rollup/plugin-typescript';
 
 export default [
 	{
-		input: './src/client/js/application.js',
+		input: './src/client/ts/application.ts',
 		output: {
 			file: './build/client/js/application.js',
 			format: 'esm'
@@ -20,12 +21,13 @@ export default [
 				compact: true,
 			}),*/
 			//image(),
+			typescript(),
 			nodeResolve(),
 			//terser(),
 			copy({
 				copyOnce: true,
 				targets: [
-					{src: 'src/client/index.html', dest: 'build/client/'},
+					{ src: 'src/client/index.html', dest: 'build/client/' },
 				]
 			}),
 		],
